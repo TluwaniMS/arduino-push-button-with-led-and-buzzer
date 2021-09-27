d44e3c892dae4cd5f78bc856c3b1e2b44c3225c4
tluwaniMS <tluwani2020@gmail.com>
2021-09-27 20:36:31 +0200

    adding-initial-sketch

---
+++ push-button-buzzer-and-led-trigger/push-button-buzzer-and-led-trigger.ino
@@ -0,0 +1,20 @@
+const int ledPin = 13; |
+const int buzzerPin = 9;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+void triggerBuzzer() {};
+
+void triggerLED() {};
+
+void checkSwitchStatus() {};
