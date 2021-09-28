f6a093a7d717df879837662d34a9e88e96973467
tluwaniMS <tluwani2020@gmail.com>
2021-09-28 06:46:28 +0200

    adding-logic-to-trigger-led-and-buzzer

--- push-button-buzzer-and-led-trigger/push-button-buzzer-and-led-trigger.ino
+++ push-button-buzzer-and-led-trigger/push-button-buzzer-and-led-trigger.ino
@@ -1,20 +1,47 @@
-const int ledPin = 13; |
+const int redLedPin = 13;
+const int greenLedPin = 12;
 const int buzzerPin = 9;
+const int switchPin = 10;
 
+int switchPinStatus = 0;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(switchPin, INPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-
+  if (switchPinStatus == HIGH)
+  {
+    for (int repetition = 0; repetition < 3; repetition++) {
+      switchOnBuzzer();
+      switchOnGreenLed();
+      delay(1000);
+      switchOffGreenLed();
+      switchOffBuzzer();
+      delay(1000);
+    }
+  }
+  else
+  {
+  }
 }
 
 
-void triggerBuzzer() {};
+void doubleBlinkRedLed() {}
 
-void triggerLED() {};
+void switchOnGreenLed() {}
 
-void checkSwitchStatus() {};
+void switchOnBuzzer() {}
+
+void switchOffRedLed() {}
+
+void switchOffGreenLed() {}
+
+void switchOffBuzzer() {}
